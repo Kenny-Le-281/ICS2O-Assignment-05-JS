b60c5a2ddda0c5b9198e5f26a7b759869cb11a00
Kenny Le <99355017+Kenny-Le-281@users.noreply.github.com>
2022-05-09 17:33:09 -0400

    init

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 // Created by: Kenny Le
-// Created on: Apr 2022
+// Created on: May 2022
 // This file contains the JS functions for index.html
 
 /**
  * Check service worker.
  */
-if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
-    scope: "/ICS2O-PWA-Test/",
+ if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-Assignment-05-JS/sw.js", {
+    scope: "/ICS2O-Assignment-05-JS/",
   })
 }
 
