0f8798c36f6113b0f0a57c10c170479ebd0a6119
Kenny Le <99355017+Kenny-Le-281@users.noreply.github.com>
2022-05-10 04:08:44 +0000

    changes in script.js

--- js/script.js
+++ js/script.js
@@ -12,8 +12,35 @@
 }
 
 /**
- * This function displays an alert.
+ * This function calculates π
  */
-function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+function calculate() {
+  // input
+  var userInput = document.getElementById("user-input").value
+
+  // process
+  let counter = 0
+  var answer = 0
+  const NUMERATOR = 4
+  var denominator = 1
+
+  if (userInput < 0)
+  {
+    document.getElementById("answer").innerHTML = "Please input a positive whole number!"
+  }
+
+  while (counter < userInput) {
+    console.log("Once through loop:" + counter)
+    if (counter % 2 == 0) {
+      denominator += 2
+      answer += NUMERATOR / denominator
+    } else if (counter % 2 == 1) {
+      denominator += 2
+      answer += - (NUMERATOR / denominator)
+    }
+    counter++
+  }
+
+  // output
+  document.getElementById("answer").innerHTML = "The value of π is " + (answer)
 }
